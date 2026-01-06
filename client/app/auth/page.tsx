813c69f2ca67fbc9c1b0af6a2577fbd513aa5397
Morshed Alam <ma.morshedalampro@gmail.com>
2026-01-06 14:21:21 +0800

    Adjust spacing in auth page components
    Reduced vertical spacing from 5 to 4 in several elements on the authentication page for a more compact layout.

--- client/app/auth/page.tsx
+++ client/app/auth/page.tsx
@@ -6,12 +6,12 @@ import Image from "next/image";
 export default function Page () {
      return (
           <section className="flex flex-col h-full px-3 pb-9 justify-end">
-               <div className="text-center space-y-5 mb-9">
+               <div className="text-center space-y-4 mb-9">
                     <Image src={ICONS.RedIcon} alt="Red Icon" width={62} height={62} className="mx-auto" />
                     <h3 className="font-bold text-4xl tracking-wide">Welcome Back!</h3>
                     <h2 className="font-semibold text-2xl">Login or sign up</h2>
                </div>
-               <form className="space-y-5">
+               <form className="space-y-4">
                     <Input type="email" placeholder="Email" />
                     <Button type="submit" className="bg-stone-100 text-black">Continue</Button>
                </form>
@@ -20,7 +20,7 @@ export default function Page () {
                     <span className="text-stone-300">or</span>
                     <hr className="w-5/12 border-b border-stone-600" />
                </div>
-               <div className="space-y-5">
+               <div className="space-y-4">
                     <Button variant="outline">
                          <Image src={ICONS.GoogleIcon} alt="Google Icon" width={24} height={24} />
                          Continue with Google
