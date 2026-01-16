927f13c93938ce3b89425388692b5907a7a140ab
Morshed Alam <ma.morshedalampro@gmail.com>
2026-01-16 11:36:43 +0800

    Add transaction page and refactor Header component
    Introduces a new transaction page with transaction listing, filters, and search functionality. Refactors the Header component to accept a title and children as props, simplifying its structure and removing dropdown and navigation logic.

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,39 +1,17 @@
-import { ChevronLeft, EllipsisVertical } from "lucide-react";
 import { Button } from "./ui/button";
-import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
 
-export default function Header() {
+type HeaderProps = {
+  title: string;
+  children: React.ReactNode;
+};
+
+export default function Header({ title, children }: HeaderProps) {
   return (
-    <header className="fixed top-0 left-0 w-full p-2 z-999">
-      <div className="flex items-center justify-between flex-wrap">
-        <Link href="/">
-          <Button variant="link" size="icon" className="x-icon-bg">
-            <ChevronLeft />
-          </Button>
-        </Link>
-        <h2 className="font-bold tracking-wide text-lg">Statistics</h2>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="link" size="icon" className="x-icon-bg">
-              <EllipsisVertical />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="start">
-            <DropdownMenuLabel>My Account</DropdownMenuLabel>
-            <DropdownMenuItem>Profile</DropdownMenuItem>
-            <DropdownMenuItem>Billing</DropdownMenuItem>
-            <DropdownMenuItem>Settings</DropdownMenuItem>
-            <DropdownMenuItem>Log out</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
+    <header className="flex items-center justify-between flex-wrap h-10 p-2">
+      <h2 className="font-bold tracking-wide text-lg">{title}</h2>
+      <Button variant="link" size="icon-sm" className="x-icon-bg">
+        {children}
+      </Button>
     </header>
   );
 }
